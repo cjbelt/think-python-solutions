06629c0499332ef10fb4b0e9eba927897be5d0b0
cjbelt <123838557+cjbelt@users.noreply.github.com>
2026-06-18 22:45:57 -0300

    Exercise 19.6 almost solved: basic text, links, images and styles implmented.

--- WebBrowser.py
+++ WebBrowser.py
@@ -1,6 +1,10 @@
 import urllib.request
+import urllib.parse
 from html.parser import HTMLParser
 from swampy.Gui import *
+import PIL.Image
+import PIL.ImageTk
+import gzip
 
 class Browser(Gui):
     def __init__(self):
@@ -17,16 +21,23 @@ class Browser(Gui):
         self.canvas = self.sc(width=600, height=1000, bg='white')
         self.canvas.width = self.canvas.canvas.get_width()
         self.canvas.height = self.canvas.canvas.get_height()
-        self.parser = BrowserParser(self.canvas)
+        self.parser = BrowserParser(self.canvas, self)
         self.load_site()
 
     def load_site(self, event=None):
         try:
-            request = urllib.request.urlopen(self.site_text.get(0.0, END))
-            content = request.read().decode('utf-8', errors='ignore')
+            request = urllib.request.Request(self.site_text.get(0.0, END).strip(), headers={'User-Agent': 'Mozilla/5.0'})
+            response = urllib.request.urlopen(request)
+            raw_data = response.read()
+
+            if response.headers.get('Content-Encoding') == 'gzip':
+                raw_data = gzip.decompress(raw_data)
+
+            content = raw_data.decode('utf-8', errors='ignore')
 
             self.canvas.canvas.clear()
             self.parser.reset_layout()
+            self.parser.images = []
             self.parser.feed(content)
             self.canvas.canvas.scroll_config()
             self.ind += 1
@@ -42,15 +53,17 @@ class Browser(Gui):
 
     def back(self, event=None):
         if self.ind > 0:
-            self.site_text.delete(0, END)
+            self.site_text.delete(0.0, END)
             self.ind -= 1
-            self.site_text.insert(0, self.history[self.ind])
+            self.site_text.insert(0.0, self.history[self.ind])
             self.load_site()
 
 class BrowserParser(HTMLParser):
-    def __init__(self, canvas):
+    def __init__(self, canvas, browser):
         super().__init__()
+        self.browser = browser
         self.canvas = canvas
+        self.images = []
         self.reset_layout()
 
     def reset_layout(self):
@@ -61,18 +74,22 @@ class BrowserParser(HTMLParser):
         self.weight = 'normal'
         self.slant = 'roman'
         self.color = 'black'
+        self.underline = False
+        self.link = False
+        self.link_href = ''
 
         self.in_script_or_style = False
 
     def handle_starttag(self, tag, attrs):
         attrs_dict = dict(attrs)
+        styles = self.parse_inline_styles(attrs_dict.get('style', ''))
 
         if tag in ('script', 'style'):
             self.in_script_or_style = True
             return
 
         if tag == 'body':
-            bg_color = attrs_dict.get('bgcolor', 'white')
+            bg_color = attrs_dict.get('bgcolor', 'white') or styles.get("background-color") or '#ffffff'
             self.canvas.canvas.config(bg=bg_color)
 
         elif tag == 'h1':
@@ -100,8 +117,27 @@ class BrowserParser(HTMLParser):
         elif tag == 'br':
             self.flush_line()
 
+        elif tag in ['p', 'div', 'h1', 'h2']:
+            if 'background-color' in styles:
+                block_color = styles['background-color']
+                self.canvas.canvas.rectangle([self.canvas.canvas.canvas_coords([0, self.cursor_y]), self.canvas.canvas.canvas_coords([0 + self.canvas.width, self.cursor_y + 120])], block_color)
+
         elif tag == 'a':
-            # TODO: add link handling
+            self.link = True
+            self.link_href = urllib.parse.urljoin(self.browser.site_text.get(0.0, END).strip(), attrs_dict.get('href', ''))
+            self.color = 'blue'
+            self.underline = True
+
+        elif tag == 'img':
+            src = attrs_dict.get('src')
+
+            if src:
+                img_url = urllib.parse.urljoin(self.browser.site_text.get(0.0, END).strip(), src)
+                self.render_image(img_url)
+
+        if 'color' in styles:
+            self.color = styles['color']
+
 
     def handle_endtag(self, tag):
         if tag in ['script', 'style']:
@@ -119,7 +155,11 @@ class BrowserParser(HTMLParser):
             self.slant = 'roman'
         elif tag in ['p', 'div']:
             self.flush_line()
-        # TODO: add link handling
+        elif tag == 'a':
+            self.link = False
+            self.link_href = ''
+            self.color = 'black'
+            self.underline = False
 
     def handle_data(self, data):
         if self.in_script_or_style:
@@ -146,21 +186,62 @@ class BrowserParser(HTMLParser):
             if self.cursor_x + word_width > self.canvas.width - 15 or word == '\n':
                 self.flush_line()
 
-            self.canvas.canvas.text(
+            ca_text = self.canvas.canvas.text(
                 [self.cursor_x, self.cursor_y],
                 text=word_to_draw,
                 font=current_font,
                 fill=self.color,
+                underline=int(self.underline),
                 anchor='nw'
             )
 
-            # TODO: add link handling
+            if self.link:
+                url = urllib.parse.urljoin(self.browser.site_text.get(0.0, END).strip(), self.link_href)
+                ca_text.bind('<Button-1>', Callable(self.enter_link, url))
+                ca_text.bind('<Enter>', self.change_cursor)
+                ca_text.bind('<Leave>', self.change_cursor)
 
             self.cursor_x += word_width
 
+    def parse_inline_styles(self, style_str):
+        styles = {}
+
+        if not style_str:
+            return styles
+
+        for item in style_str.split(';'):
+            if ':' in item:
+                key, value = item.split(':', 1)
+                styles[key.strip().lower()] = value.strip().lower()
+
+        return styles
+
     def flush_line(self):
-        self.cursor_x = self.canvas.canvas.canvasx(15)
+        self.cursor_x = -self.canvas.width/2 + 15
         self.cursor_y -= self.line_height
+
+    def render_image(self, url):
+        req = urllib.request.urlopen(url)
+        photo = PIL.ImageTk.PhotoImage(PIL.Image.open(req))
+        self.images.append(photo)
+
+        if self.cursor_x + photo.width() > self.canvas.width - 15:
+            self.flush_line()
+
+        self.canvas.canvas.image([self.cursor_x, self.cursor_y], image=photo, anchor='nw')
+        self.cursor_y -= photo.height() + 10
+        self.flush_line()
+
+    def enter_link(self, url, event=None):
+        self.browser.site_text.delete(0.0, END)
+        self.browser.site_text.insert(0.0, url)
+        self.browser.load_site()
+
+    def change_cursor(self, event=None):
+        if '7' in event.type:
+            self.canvas.canvas.configure(cursor='hand2')
+        else:
+            self.canvas.canvas.configure(cursor='')
 
 if __name__ == '__main__':
     # request = urllib.request.urlopen('https://www.google.com')
